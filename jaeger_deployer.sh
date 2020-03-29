c24509c4d36995f7ff33901e0c773032a34ae8ac
vipinvkmenon <vipin.vijay.kumar9@gmail.com>
2020-03-29 16:05:59 +0530

    Init

--- jaeger_deployer.sh
+++ jaeger_deployer.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-
-
-
-
-
 es_password=$(kubectl get secret passwords-store -o jsonpath='{$.data.writer}' | base64 --decode)
 
-
 until $(curl --output /dev/null --silent --head --fail -u "writer:$es_password" http://elastic-client:9200); do es_password=$(kubectl get secret passwords-store -o jsonpath='{$.data.writer}' | base64 --decode); echo $es_password; sleep 5;  done; echo \"Complete\";
-
 
 
 depl="{\"apiVersion\":\"jaegertracing.io/v1\",\"kind\":\"Jaeger\",\"metadata\":{\"name\":\"jaeger\"},\"spec\":{\"strategy\":\"production\",\"storage\":{\"type\":\"elasticsearch\",\"options\":{\"es\":{\"server-urls\":\"http://elastic-client:9200\",\"username\":\"writer\",\"password\":\"$es_password\"}}}}}"
