b4abff475d29cadefa4a4109b96af4ba4cf5cd5e
Vipin Menon <vipinvkmenon@users.noreply.github.com>
2020-03-30 20:21:03 +0530

    Update jaeger_deployer.sh

--- jaeger_deployer.sh
+++ jaeger_deployer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 es_password=$(kubectl get secret passwords-store -o jsonpath='{$.data.admin}' | base64 --decode)
+ns=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
+instance=$(echo "$ns" | cut -c 4-)
 
 until $(curl --output /dev/null --silent --head --fail -u "admi:$es_password" http://elastic-client:9200); do es_password=$(kubectl get secret passwords-store -o jsonpath='{$.data.admin}' | base64 --decode); echo $es_password; sleep 5;  done; echo \"Complete\";
 
