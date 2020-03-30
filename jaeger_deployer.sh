b01069922788f1c82b6362e7fa718ee4d44d8a48
Vipin Menon <vipinvkmenon@users.noreply.github.com>
2020-03-30 14:07:58 +0530

    Update jaeger_deployer.sh

--- jaeger_deployer.sh
+++ jaeger_deployer.sh
@@ -2,7 +2,7 @@
 
 es_password=$(kubectl get secret passwords-store -o jsonpath='{$.data.admin}' | base64 --decode)
 
-until $(curl --output /dev/null --silent --head --fail -u "writer:$es_password" http://elastic-client:9200); do es_password=$(kubectl get secret passwords-store -o jsonpath='{$.data.admin}' | base64 --decode); echo $es_password; sleep 5;  done; echo \"Complete\";
+until $(curl --output /dev/null --silent --head --fail -u "admin:$es_password" http://elastic-client:9200); do es_password=$(kubectl get secret passwords-store -o jsonpath='{$.data.admin}' | base64 --decode); echo $es_password; sleep 5;  done; echo \"Complete\";
 
 
 depl="{\"apiVersion\":\"jaegertracing.io/v1\",\"kind\":\"Jaeger\",\"metadata\":{\"name\":\"jaeger\"},\"spec\":{\"strategy\":\"production\",\"storage\":{\"type\":\"elasticsearch\",\"options\":{\"es\":{\"server-urls\":\"http://elastic-client:9200\",\"username\":\"admin\",\"password\":\"$es_password\"}}}}}"
