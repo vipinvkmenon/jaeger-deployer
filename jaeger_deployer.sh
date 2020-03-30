0e2649998459f64b85b25ef83e851d550fa7035f
Vipin Menon <vipinvkmenon@users.noreply.github.com>
2020-03-30 20:28:13 +0530

    Update jaeger_deployer.sh

--- jaeger_deployer.sh
+++ jaeger_deployer.sh
@@ -3,6 +3,7 @@
 es_password=$(kubectl get secret passwords-store -o jsonpath='{$.data.admin}' | base64 --decode)
 ns=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 instance=$(echo "$ns" | cut -c 4-)
+ingress_val=$(kubectl get eckf "eckf-$instance" -o jsonpath='{$.spec.ingress}')
 
 until $(curl --output /dev/null --silent --head --fail -u "admi:$es_password" http://elastic-client:9200); do es_password=$(kubectl get secret passwords-store -o jsonpath='{$.data.admin}' | base64 --decode); echo $es_password; sleep 5;  done; echo \"Complete\";
 
@@ -13,4 +14,8 @@ depl=$(echo $depl)
 cat << EOF | kubectl apply -f -
 $depl
 EOF
+
+##
+collector_url="collector-$instance.$ingress_val"
+cd tmp
 
