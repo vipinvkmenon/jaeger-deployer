acf1e3485066728feea384026bb477914f3ee5e0
Vipin Menon <vipinvkmenon@users.noreply.github.com>
2020-03-31 16:12:41 +0530

    Update jaeger_deployer.sh

--- jaeger_deployer.sh
+++ jaeger_deployer.sh
@@ -5,7 +5,7 @@ ns=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 instance=$(echo "$ns" | cut -c 4-)
 ingress_val=$(kubectl get eckf "eckf-$instance" -o jsonpath='{$.spec.ingress}')
 
-until $(curl --output /dev/null --silent --head --fail -u "admi:$es_password" http://elastic-client:9200); do es_password=$(kubectl get secret passwords-store -o jsonpath='{$.data.admin}' | base64 --decode); echo $es_password; sleep 5;  done; echo \"Complete\";
+until $(curl --output /dev/null --silent --head --fail -u "admin:$es_password" http://elastic-client:9200); do es_password=$(kubectl get secret passwords-store -o jsonpath='{$.data.admin}' | base64 --decode); echo $es_password; sleep 5;  done; echo \"Complete\";
 
 
 depl="{\"apiVersion\":\"jaegertracing.io/v1\",\"kind\":\"Jaeger\",\"metadata\":{\"name\":\"jaeger\"},\"spec\":{\"strategy\":\"production\",\"storage\":{\"type\":\"elasticsearch\",\"options\":{\"es\":{\"server-urls\":\"http://elastic-client:9200\",\"username\":\"admin\",\"password\":\"$es_password\"}}}}}"
